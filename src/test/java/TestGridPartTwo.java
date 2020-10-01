a7b302e03e7e1f6d396693367439e72c54dd92e7
Zahid Dawod <zahiddawod@hotmail.com>
2020-10-01 15:05:32 -0400

    TEST: Monkey Business (tests 85-87)

--- src/test/java/TestGridPartTwo.java
+++ src/test/java/TestGridPartTwo.java
@@ -47,4 +47,46 @@ public class TestGridPartTwo {
         assertEquals(true, game.RollDices(new int[]{1,2}));
         assertEquals(600, game.UpdateScore());
     }
+
+    @Test
+    public void Test85() {
+        Game game = new Game();
+        game.SetDrawnCard(FortuneCard.MonkeyBusiness);
+        game.desiredDices = new ArrayList<Dice>() {{ add(Dice.Parrot); add(Dice.Parrot); add(Dice.Parrot); add(Dice.Monkey); add(Dice.Monkey); add(Dice.Monkey); add(Dice.Skull); add(Dice.Coin); }};
+        assertEquals(true, game.RollDices(new int[]{1,2,3,4,5,6,7,8}));
+        assertEquals(1100, game.UpdateScore());
+    }
+
+    @Test
+    public void Test86() {
+        Game game = new Game();
+        game.SetDrawnCard(FortuneCard.MonkeyBusiness);
+        game.desiredDices = new ArrayList<Dice>() {{ add(Dice.Parrot); add(Dice.Coin); add(Dice.Parrot); add(Dice.Sword); add(Dice.Sword); add(Dice.Monkey); add(Dice.Diamond); add(Dice.Sword); }};
+        assertEquals(true, game.RollDices(new int[]{1,2,3,4,5,6,7,8}));
+        assertEquals(900, game.UpdateScore());
+        game.desiredDices = new ArrayList<Dice>() {{ add(Dice.Monkey); add(Dice.Diamond); }};
+        assertEquals(true, game.RollDices(new int[]{5,8}));
+        assertEquals(500, game.UpdateScore());
+        game.desiredDices = new ArrayList<Dice>() {{ add(Dice.Coin); add(Dice.Sword); }};
+        assertEquals(true, game.RollDices(new int[]{3,7}));
+        assertEquals(400, game.UpdateScore());
+    }
+
+    @Test
+    public void Test87() {
+        Game game = new Game();
+        game.SetDrawnCard(FortuneCard.MonkeyBusiness);
+        game.desiredDices = new ArrayList<Dice>() {{ add(Dice.Coin); add(Dice.Coin); add(Dice.Coin); add(Dice.Coin); add(Dice.Coin); add(Dice.Coin); add(Dice.Coin); add(Dice.Coin); }};
+        assertEquals(true, game.RollDices(new int[]{1,2,3,4,5,6,7,8}));
+        assertEquals(5300, game.UpdateScore());
+        game.desiredDices = new ArrayList<Dice>() {{ add(Dice.Monkey); add(Dice.Monkey); add(Dice.Monkey); }};
+        assertEquals(true, game.RollDices(new int[]{1,2,3}));
+        assertEquals(1600, game.UpdateScore());
+        game.desiredDices = new ArrayList<Dice>() {{ add(Dice.Sword); add(Dice.Sword); }};
+        assertEquals(true, game.RollDices(new int[]{7,8}));
+        assertEquals(500, game.UpdateScore());
+        game.desiredDices = new ArrayList<Dice>() {{ add(Dice.Parrot); add(Dice.Parrot); add(Dice.Parrot); add(Dice.Parrot); }};
+        assertEquals(true, game.RollDices(new int[]{4,5,6,7}));
+        assertEquals(2000, game.UpdateScore());
+    }
 }
